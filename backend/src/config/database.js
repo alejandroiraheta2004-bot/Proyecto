4c30fe40f0a808ed9ee31b412bb4db3fd0a1c220
PC <alejandroiraheta2004@gmail.com>
2026-02-02 20:37:53 -0800

    Fix DATABASE_URL fallback

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,16 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+dotenv.config();
 
+const pool = process.env.DATABASE_URL
+	? mysql.createPool(process.env.DATABASE_URL)
+	: mysql.createPool({
+			host: process.env.DB_HOST || '${{RAILWAY_PRIVATE_DOMAIN}}',
+			user: process.env.DB_USER || 'root',
+			password: process.env.DB_PASSWORD || '${{MYSQL_ROOT_PASSWORD}}',
+			database: process.env.DB_NAME || 'railway',
+			waitForConnections: true,
+			connectionLimit: 10
+		});
 export default pool;
